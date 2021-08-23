0df0ab35346e5e644a116555347123f0aa48d7ab
Okky Surya Lesmana <realmeokky@gmail.com>
2021-08-23 14:49:20 +0700

    Update All

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+use App\Models\Penduduk;
 
 class DatabaseSeeder extends Seeder
 {
